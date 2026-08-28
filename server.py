8757ea11eff535d9aed2d63d0768f263d95cfd92
maznshwa51-tech <maznshwa51@gmail.com>
2026-08-28 15:19:26 +0400

    Update server.py

--- server.py
+++ server.py
@@ -16,10 +16,10 @@ from datetime import datetime, timezone, timedelta
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
-
+db_name = os.environ.get('DB_NAME', 'halawayat_attar')
+db = client[db_name]
 ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL', '').strip().lower()
 APP_NAME = os.environ.get('APP_NAME', 'halawayat-attar')
 EMERGENT_KEY = os.environ.get('EMERGENT_LLM_KEY')
